4908b7be6d409b36eeee7cdeacb765a38b3fc3c7
Anqush Jamdagani <njamdagani@gmail.com>
2017-08-23 19:06:09 +0530

    basic clock completed

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ function setDate()
 	var seconds = a.getSeconds();
 	var secondDegrees = seconds * 6;
 	document.querySelector("#second").style.transform = `rotate(${secondDegrees}deg)`;
-	var minute = a.getMinutes();
+	var minuteDegrees = seconds * 1/10;
+	document.querySelector("#minute").style.transform = `rotate(${minuteDegrees}deg`;
+	var hourDegrees = hour * 1/120 ;
+	document.querySelector("#hour").style.transform = `rotate(${hourDegrees}deg`;
 }
 
 setInterval(setDate,1000);
