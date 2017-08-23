b2b32191d1957863179584578aa29af99d63ba91
Anqush Jamdagani <njamdagani@gmail.com>
2017-08-24 01:00:46 +0530

    change color with time

--- js/main.js
+++ js/main.js
@@ -8,8 +8,10 @@ function setDate()
 	var minuteDegrees = minute * 6;
 	document.querySelector("#minute").style.transform = `rotate(${minuteDegrees}deg`;
 	var hour = a.getHours();
-	var hourDegrees = hour * 30 ;
+	var hourDegrees = hour * 30;
 	document.querySelector("#hour").style.transform = `rotate(${hourDegrees}deg`;
+	document.querySelector("#wrapper").style.backgroundColor = `rgb(${seconds*2},${seconds*4},${seconds*3})`;
+	console.log(`rgb(${seconds*2},${seconds*4},${seconds*3})`);
 }
 
 setInterval(setDate,1000);
